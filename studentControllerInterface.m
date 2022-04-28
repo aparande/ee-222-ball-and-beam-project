d106277a7eec0d816f22fe7df1883e4e53718c32
Anmol Parande <parandea1.7@gmail.com>
2022-04-28 10:10:02 -0700

    Dynamic extension V1

--- studentControllerInterface.m
+++ studentControllerInterface.m
@@ -4,9 +4,21 @@ classdef studentControllerInterface < matlab.System
         % For more information of the supported data type, see
         % https://www.mathworks.com/help/simulink/ug/data-types-supported-by-simulink.html
         t_prev = -1;
+		theta_prev = 0;
+        pos_prev = 0;
+        vel_prev = 0;
+        
         theta_d = 0;
         extra_dummy1 = 0;
         extra_dummy2 = 0;
+        virtual_u = 0;
+
+		% System Parameters
+		r_arm = 0.0254;
+		L = 0.4255;
+		g = 9.81;
+		K = 1.5;
+		tau = 0.025;
     end
     methods(Access = protected)
         % function setupImpl(obj)
@@ -24,29 +36,45 @@ classdef studentControllerInterface < matlab.System
         %   theta: servo motor angle provided by the encoder of the motor (rad)
         % Output:
         %   V_servo: voltage to the servo input.        
-            %% Sample Controller: Simple Proportional Controller
-            t_prev = obj.t_prev;
+            theta_saturation = 56 * pi / 180;
+            k_p = 2;
+            k_v = 1;
+            k_u = 2;
+
+            a = 5 * obj.g * obj.r_arm / (7 * obj.L);
+            b = (5 * obj.L / 14) * (obj.r_arm / obj.L)^2;
+            c = (5 / 7) * (obj.r_arm / obj.L)^2;
+
             % Extract reference trajectory at the current timestep.
             [p_ball_ref, v_ball_ref, a_ball_ref] = get_ref_traj(t);
-            % Decide desired servo angle based on simple proportional feedback.
-            k_p = 3;
-            theta_d = - k_p * (p_ball - p_ball_ref);
 
-            % Make sure that the desired servo angle does not exceed the physical
-            % limit. This part of code is not necessary but highly recommended
-            % because it addresses the actual physical limit of the servo motor.
-            theta_saturation = 56 * pi / 180;    
-            theta_d = min(theta_d, theta_saturation);
-            theta_d = max(theta_d, -theta_saturation);
+            vel = (p_ball - obj.pos_prev) / 0.01;
+            dtheta = (theta - obj.theta_prev) / 0.01;
 
-            % Simple position control to control servo angle to the desired
-            % position.
-            k_servo = 10;
-            V_servo = k_servo * (theta_d - theta);
+		    u_d = 1 ./ (b - c .* p_ball) .* (a .* sin(theta) + k_v * (vel - v_ball_ref) + k_p .* (p_ball - p_ball_ref));
+            z = obj.tau / ( 2 * obj.K .* cos(theta) .^ 2);
+
+            if dtheta < 1
+                dtheta = 1;
+            end
+
+            z = z ./ dtheta;
+            
+            u_tilde = (dtheta .* cos(theta)) .^ 2;
+
+		    u =  z * (2 / obj.tau * u_tilde + dtheta .^ 3 .* sin(2 .* theta) - k_u .* (u_d - u_tilde));
+            if abs(u) < 10
+                V_servo = u;
+            else
+                V_servo = sign(u) * 10;
+            end
             
             % Update class properties if necessary.
             obj.t_prev = t;
-            obj.theta_d = theta_d;
+            obj.theta_d = 0;
+			obj.theta_prev = theta;
+            obj.pos_prev = p_ball;
+            obj.vel_prev = vel;
         end
     end
     
